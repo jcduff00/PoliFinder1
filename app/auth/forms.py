0e6c1fd0ebf24353afd89abee2c21c5d4e4903c4
jcduff00 <johncduffman@gmail.com>
2023-02-28 18:30:54 -0500

    created auth routes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,12 +13,12 @@ class SignUpForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError('That username is taken by another user.')
 
 
 class LoginForm(FlaskForm):
     username = StringField('User Name',
-        validators=[DataRequired(), Length(min=3, max=50)])
+        validators=[DataRequired(), Length(50)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
 
